013aa24329a923d63448898e03f04c77d506ff44
rektide <rektide@voodoowarez.com>
2016-04-17 17:34:37 -0400

    Continuous reservoir sampling.

---
+++ rxjs-reservoir-sampler.js
@@ -0,0 +1,30 @@
+var
+  weightedReservoirSampler= require( "weighted-reservoir-sampler")
+
+function itemWeightProp(i){
+	if(i && !isNaN( i.weight)){
+		return i.weight
+	}
+	return i
+}
+
+function reservoirSampler( opts){
+	var
+	  weightFunction: o.weightFunction|| itemWeightProp,
+	  n: opts.n|| 1,
+	  samplerOpts= {weightFunction, sampleSize: n},
+	  sampler= weightedReservoirSampler( samplerOpts),
+	  push= sampler.push.bind( sampler),
+	  subject= new Rx.BehaviorSubject(),
+	  started= false
+	opts.src.subscribe(function( x)[
+		sampler.push( x)
+		if(started){
+			subject.next( sampler.read())
+		}
+	})
+	opts.src.take( 3).subscribe( null, null, function(){
+		started= true
+	})
+	return subject
+}
